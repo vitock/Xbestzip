2211ef3d44c3297e2a97c7241daaf4e6aa94a6bd
liw <liw>
2019-10-16 13:07:44 +0800

    Initial Commit

---
+++ XXBestZipXX/XXBestZipXX.h
@@ -0,0 +1,19 @@
+//
+//  XXBestZipXX.h
+//  XXBestZipXX
+//
+//  Created by wei li on 2019/10/16.
+//  Copyright © 2019 LJ. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XXBestZipXX.
+FOUNDATION_EXPORT double XXBestZipXXVersionNumber;
+
+//! Project version string for XXBestZipXX.
+FOUNDATION_EXPORT const unsigned char XXBestZipXXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XXBestZipXX/PublicHeader.h>
+
+
